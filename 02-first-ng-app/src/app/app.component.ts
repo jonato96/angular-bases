385dce45bf37980d155e3edea5e539cf96509d23
Jonathan Sánchez <jonathan.sanchez9@zurich.com>
2024-04-20 20:19:05 -0500

    clean-app

--- 02-first-ng-app/src/app/app.component.ts
+++ 02-first-ng-app/src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = '02-first-ng-app';
+  public title: string = 'Hola Mundo';
+  public counter: number = 10;
 }
