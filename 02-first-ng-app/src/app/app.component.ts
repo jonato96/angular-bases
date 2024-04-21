f277a389be16dbe065d61954447b3dac2cae09a7
Jonathan Sánchez <jonathan.sanchez9@zurich.com>
2024-04-20 20:31:01 -0500

    add-counter

--- 02-first-ng-app/src/app/app.component.ts
+++ 02-first-ng-app/src/app/app.component.ts
@@ -8,4 +8,11 @@ import { Component } from '@angular/core';
 export class AppComponent {
   public title: string = 'Hola Mundo';
   public counter: number = 10;
+
+  increaseBy(value: number): void {
+    this.counter += value;
+  }
+  resetCounter():void{
+    this.counter = 10;
+  }  
 }
