43ea7438aabf1e91c47d4a9b2c90b83ea6e81e03
Jonathan Sánchez <jonathan.sanchez9@zurich.com>
2024-04-30 10:33:20 -0500

    learn-input

--- 02-first-ng-app/src/app/dbz/pages/main-page.component.ts
+++ 02-first-ng-app/src/app/dbz/pages/main-page.component.ts
@@ -1,12 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+import { Character } from '../interfaces/character.interface';
+
 
 @Component({
     selector: 'app-dbz-main-page',
     templateUrl: './main-page.component.html'
 })
 
-export class MainPageComponent implements OnInit {
-    constructor() { }
-
-    ngOnInit() { }
+export class MainPageComponent {
+    public character: Character[] = [
+        {
+            name: "krillin",
+            power: 100
+        },
+        {
+            name: "goku",
+            power: 9000   
+        },
+        {
+            name: "vegeta",
+            power:7500
+        }
+    ];
 }
