41f0cbb408c56493fbf666b55227430464947cfb
jonato96 <jonato96s@gmail.com>
2024-05-04 15:47:43 -0500

    format

--- 02-first-ng-app/src/app/dbz/components/add-character/add-character.component.ts
+++ 02-first-ng-app/src/app/dbz/components/add-character/add-character.component.ts
@@ -18,9 +18,11 @@ export class AddCharacterComponent {
 
   emitCharacter(): void {
     console.log(this.character);
+    
     if(this.character.name.length === 0){
       return;
     }
+
     this.onNewCharacter.emit(this.character);
     // this.onNewCharacter.emit({...this.character});
     this.character.name = "";
