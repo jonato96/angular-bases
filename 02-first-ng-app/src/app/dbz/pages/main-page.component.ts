4558a50397bc027108d04008e08f78c10d05ed52
Jonathan Sánchez <jonathan.sanchez9@zurich.com>
2024-05-04 15:21:44 -0500

    learn-output-decorator

--- 02-first-ng-app/src/app/dbz/pages/main-page.component.ts
+++ 02-first-ng-app/src/app/dbz/pages/main-page.component.ts
@@ -23,4 +23,9 @@ export class MainPageComponent {
             power:7500
         }
     ];
+
+    onNewCharacter(character: Character): void {
+        console.log('Main Page');
+        console.log(character);
+    }
 }
