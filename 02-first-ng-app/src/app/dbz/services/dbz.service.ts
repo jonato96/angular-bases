429ce00901188f6cc4109e11a84909706d7cac66
jonato96 <jonato96s@gmail.com>
2024-05-04 21:50:18 -0500

    add-uui-refactor

--- 02-first-ng-app/src/app/dbz/services/dbz.service.ts
+++ 02-first-ng-app/src/app/dbz/services/dbz.service.ts
@@ -1,30 +1,47 @@
 import { Injectable } from '@angular/core';
+
 import { Character } from '../interfaces/character.interface';
+
+import { v4 as uuid } from 'uuid';
+
 
 @Injectable({providedIn: 'root'})
 export class DbzService {    
 
     public character: Character[] = [
         {
+            id: uuid(),
             name: "krillin",
             power: 100
         },
         {
+            id: uuid(),
             name: "goku",
             power: 9000   
         },
         {
+            id: uuid(),
             name: "vegeta",
             power:7500
         }
     ];
 
-    onNewCharacter(character: Character): void {        
-        this.character.push(character);
+    onNewCharacter(character: Character): void { 
+        
+        const newCharacter: Character = {
+            id: uuid(),
+            ...character
+        }
+        
+        this.character.push(newCharacter);
     }
 
-    onDeleteCharacter(index: number): void {
-        this.character.splice(index, 1);
+    // onDeleteCharacter(index: number): void {
+    //     this.character.splice(index, 1);
+    // }
+
+    onDeleteCharacterById(id: string): void {
+        this.character = this.character.filter(c => c.id !== id);
     }
     
 }
