cda44009523d3414927a0f2054684fae80485adf
jonato96 <jonato96s@gmail.com>
2024-05-15 20:00:42 -0500

    adding-router

--- 04-country-app/src/app/app.module.ts
+++ 04-country-app/src/app/app.module.ts
@@ -11,8 +11,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule,
     AppRoutingModule,
+    BrowserModule,
     SharedModule
   ],
   providers: [],
