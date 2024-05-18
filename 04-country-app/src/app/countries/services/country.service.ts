677c391876762b5b7cd1a3aed2112cec2ea649f4
jonato96 <jonato96s@gmail.com>
2024-05-18 15:59:57 -0500

    add-catch-error

--- 04-country-app/src/app/countries/services/country.service.ts
+++ 04-country-app/src/app/countries/services/country.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, catchError, of } from 'rxjs';
+
 import { Country } from '../interfaces/country';
 
 
@@ -12,7 +13,10 @@ export class CountriesService {
     private url: string = 'https://restcountries.com/v3.1';
 
     searchCapital(term: string): Observable<Country[]> {
-        return this.http.get<Country[]>(`${ this.url }/capital/${ term }`);
+        return this.http.get<Country[]>(`${ this.url }/capital/${ term }`)
+        .pipe(
+            catchError( () => of([]))
+        );
     }
     
 }
