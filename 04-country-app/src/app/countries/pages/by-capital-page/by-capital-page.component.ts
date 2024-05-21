ed5ffbae06659ceca924844f43e06c45bad94c2c
jonato96 <jonato96s@gmail.com>
2024-05-21 08:36:23 -0500

    add-loading-flag

--- 04-country-app/src/app/countries/pages/by-capital-page/by-capital-page.component.ts
+++ 04-country-app/src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -12,11 +12,14 @@ export class ByCapitalPageComponent {
   private countryService = inject(CountriesService);
 
   public countries: Country[] = [];
+  public isLoading: boolean = false;
 
   searchByCapital(term: string): void {
+    this.isLoading = true;
     this.countryService.searchCapital(term)
     .subscribe( c => {
       this.countries = c;
+      this.isLoading = false;
     });
   }
 
