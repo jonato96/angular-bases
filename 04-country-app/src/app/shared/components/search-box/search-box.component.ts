7888b6f7f6539c8e5bc696a215657e5470e29c91
jonato96 <jonato96s@gmail.com>
2024-05-22 14:50:47 -0500

    add-unsusbribe

--- 04-country-app/src/app/shared/components/search-box/search-box.component.ts
+++ 04-country-app/src/app/shared/components/search-box/search-box.component.ts
@@ -1,36 +1,39 @@
-import { Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
-import { Subject, SubjectLike, debounceTime } from 'rxjs';
+import { Component, ElementRef, EventEmitter, Input, OnDestroy, OnInit, Output, ViewChild } from '@angular/core';
+import { Subject, SubjectLike, Subscription, debounceTime } from 'rxjs';
 
 @Component({
   selector: 'shared-search-box',
   templateUrl: './search-box.component.html',
   styles: ``
 })
-export class SearchBoxComponent implements OnInit{
-
+export class SearchBoxComponent implements OnInit, OnDestroy{
+  
   
   private debouncer: Subject<string> = new Subject();
+  private debouncerSuscription?: Subscription;
   
   @Input()
   public placeholder: string = "";
   
   // @Output()
   // public onValue = new EventEmitter<string>();
-
+  
   @Output()
   public onDebounce = new EventEmitter<string>();
   
   ngOnInit(): void {
-    this.debouncer
+    this.debouncerSuscription = this.debouncer
     .pipe(
       debounceTime(1000)
     )
-    .subscribe(
-      value => {
-        this.onDebounce.emit(value);
-      }
+    .subscribe( value => this.onDebounce.emit(value)      
     );
   }
+
+  ngOnDestroy(): void {
+    this.debouncerSuscription?.unsubscribe();
+  }
+
   // @ViewChild('txtInput')
   // public tagInput!: ElementRef<HTMLInputElement>
 
