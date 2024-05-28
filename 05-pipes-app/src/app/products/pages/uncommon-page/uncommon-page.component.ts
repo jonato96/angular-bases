05e52ca7026f07eb5cc9dc4983ee22d22fb4d262
jonato96 <jonato96s@gmail.com>
2024-05-28 08:31:08 -0500

    add-i18nSelect-pipe

--- 05-pipes-app/src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ 05-pipes-app/src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class UncommonPageComponent {
 
+  // i18n Select
+  public name: string = 'Jonathan';
+  public gender: 'male' | 'female' = 'male';
+  public invitationMap = {
+    'male': 'invitarlo',
+    'female': 'invitarla'
+  }
+
+  changeClient(): void {
+    this.name = 'Elena';
+    this.gender = 'female';
+  }
+
 }
