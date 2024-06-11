96b6aeed5f8c59fa729a65e3c82c22beb9053409
jonato96 <jonato96s@gmail.com>
2024-06-11 09:08:24 -0500

    add-routes

--- 07-form-app/src/app/auth/auth-routing.module.ts
+++ 07-form-app/src/app/auth/auth-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegiterPageComponent } from './pages/regiter-page/regiter-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: 'sign-up',
+        component: RegiterPageComponent
+      },
+      {
+        path: '**',
+        redirectTo: 'sign-up'
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
