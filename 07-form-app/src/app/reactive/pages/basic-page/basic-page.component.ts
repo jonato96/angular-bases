a3cdee6c65f59dae708ea6a911723a56f96d68e0
jonato96 <jonato96s@gmail.com>
2024-06-11 20:31:57 -0500

    add-basic-validations

--- 07-form-app/src/app/reactive/pages/basic-page/basic-page.component.ts
+++ 07-form-app/src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, inject } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './basic-page.component.html',
@@ -16,12 +16,13 @@ export class BasicPageComponent {
   // });
 
   public myForm: FormGroup = this.fb.group({
-    name: [''],
-    price: [0],
-    inStorage: [0]
+    name: ['', [ Validators.required, Validators.minLength(3) ]],
+    price: [0, [ Validators.required, Validators.min(0) ]],
+    inStorage: [0, [ Validators.required, Validators.min(0) ]]
   });
 
   onSave(): void {
+    if (this.myForm.invalid) return;
     console.log(this.myForm.value);
   }
 
