f78ae2b185b5b88c8bbf03526efd80619a2b8ad0
jonato96 <jonato96s@gmail.com>
2024-06-11 21:21:29 -0500

    add-reset

--- 07-form-app/src/app/reactive/pages/basic-page/basic-page.component.ts
+++ 07-form-app/src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -24,6 +24,7 @@ export class BasicPageComponent {
   onSave(): void {
     if (this.myForm.invalid) return;
     console.log(this.myForm.value);
+    this.myForm.reset({name: '', price: 0, inStorage: 0});
   }
 
 }
