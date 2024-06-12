d25fd828edf16d23e06702e28f284d76267adc81
jonato96 <jonato96s@gmail.com>
2024-06-11 20:04:03 -0500

    add-basic-form

--- 07-form-app/src/app/reactive/pages/basic-page/basic-page.component.ts
+++ 07-form-app/src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,9 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   templateUrl: './basic-page.component.html',
   styles: ``
 })
 export class BasicPageComponent {
+
+  private fb = inject(FormBuilder);
+
+  // public myForm: FormGroup = new FormGroup({
+  //   name: new FormControl(''),
+  //   price: new FormControl(0),
+  //   inStorage: new FormControl(0),
+  // });
+
+  public myForm: FormGroup = this.fb.group({
+    name: [''],
+    price: [0],
+    inStorage: [0]
+  });
+
+  onSave(): void {
+    console.log(this.myForm.value);
+  }
 
 }
