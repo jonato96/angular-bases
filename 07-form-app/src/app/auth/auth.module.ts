13ba61b1be6e6129300271cc7006dc7956d5fb89
jonato96 <jonato96s@gmail.com>
2024-06-14 09:22:43 -0500

    add-register-form

--- 07-form-app/src/app/auth/auth.module.ts
+++ 07-form-app/src/app/auth/auth.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { RegiterPageComponent } from './pages/regiter-page/regiter-page.component';
@@ -11,7 +12,8 @@ import { RegiterPageComponent } from './pages/regiter-page/regiter-page.componen
   ],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class AuthModule { }
--- 07-form-app/src/app/auth/pages/regiter-page/regiter-page.component.ts
+++ 07-form-app/src/app/auth/pages/regiter-page/regiter-page.component.ts
@@ -1,9 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './regiter-page.component.html',
   styles: ``
 })
 export class RegiterPageComponent {
+
+  private fb = inject(FormBuilder);
+
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required]],
+    email: ['', [Validators.required]],
+    username: ['', [Validators.required]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+    password2: ['', [Validators.required]]
+  });
+
+
+  isValidField() {
+    // TODO: obtener validacion desde un servicio
+  }
+
+  onSubmit() {
+    this.myForm.markAllAsTouched();
+  }
 
 }
