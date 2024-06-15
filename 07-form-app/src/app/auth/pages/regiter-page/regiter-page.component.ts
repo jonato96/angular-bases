ef3e0cc6f0a138bc9b007b7a363d186b40a56948
jonato96 <jonato96s@gmail.com>
2024-06-15 15:43:26 -0500

    add-email-pattern

--- 07-form-app/src/app/auth/pages/regiter-page/regiter-page.component.ts
+++ 07-form-app/src/app/auth/pages/regiter-page/regiter-page.component.ts
@@ -1,6 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { cantBeStrider } from '../../../shared/validators/validators';
+import * as customValidators from '../../../shared/validators/validators';
 
 @Component({
   templateUrl: './regiter-page.component.html',
@@ -11,11 +11,11 @@ export class RegiterPageComponent {
   private fb = inject(FormBuilder);
 
   public myForm: FormGroup = this.fb.group({
-    name: ['', [Validators.required]],
-    email: ['', [Validators.required]],
-    username: ['', [Validators.required, cantBeStrider]],
-    password: ['', [Validators.required, Validators.minLength(6)]],
-    password2: ['', [Validators.required]]
+    name: ['', [ Validators.required, Validators.pattern(customValidators.firstNameAndLastnamePattern)] ],
+    email: ['', [ Validators.required, Validators.pattern(customValidators.emailPattern)] ],
+    username: ['', [ Validators.required, customValidators.cantBeStrider] ],
+    password: ['', [ Validators.required, Validators.minLength(6)] ],
+    password2: ['', [ Validators.required] ]
   });
 
 
