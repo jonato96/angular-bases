638864e36e0e4c0217d1e7712b29f3faa3fdf8fa
jonato96 <jonato96s@gmail.com>
2024-06-15 19:21:41 -0500

    add-validator-service

--- 07-form-app/src/app/auth/pages/regiter-page/regiter-page.component.ts
+++ 07-form-app/src/app/auth/pages/regiter-page/regiter-page.component.ts
@@ -1,6 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import * as customValidators from '../../../shared/validators/validators';
+import { ValidatorsService } from '../../../shared/services/validators.service';
 
 @Component({
   templateUrl: './regiter-page.component.html',
@@ -9,18 +9,19 @@ import * as customValidators from '../../../shared/validators/validators';
 export class RegiterPageComponent {
 
   private fb = inject(FormBuilder);
+  private validatorsService = inject(ValidatorsService);
 
   public myForm: FormGroup = this.fb.group({
-    name: ['', [ Validators.required, Validators.pattern(customValidators.firstNameAndLastnamePattern)] ],
-    email: ['', [ Validators.required, Validators.pattern(customValidators.emailPattern)] ],
-    username: ['', [ Validators.required, customValidators.cantBeStrider] ],
+    name: ['', [ Validators.required, Validators.pattern(this.validatorsService.firstNameAndLastnamePattern)] ],
+    email: ['', [ Validators.required, Validators.pattern(this.validatorsService.emailPattern)] ],
+    username: ['', [ Validators.required, this.validatorsService.cantBeStrider] ],
     password: ['', [ Validators.required, Validators.minLength(6)] ],
     password2: ['', [ Validators.required] ]
   });
 
 
-  isValidField() {
-    // TODO: obtener validacion desde un servicio
+  isValidField( field: string ) {
+    return this.validatorsService.isValidField(this.myForm, field)
   }
 
   onSubmit() {
