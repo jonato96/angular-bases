65533745751c35b7263c6b650e375d21f42570a6
jonato96 <jonato96s@gmail.com>
2024-06-17 09:04:32 -0500

    add-async-email-validator

--- 07-form-app/src/app/auth/pages/regiter-page/regiter-page.component.ts
+++ 07-form-app/src/app/auth/pages/regiter-page/regiter-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ValidatorsService } from '../../../shared/services/validators.service';
+import { EmailValidatorService } from '../../../shared/validators/email-validator.service';
 
 @Component({
   templateUrl: './regiter-page.component.html',
@@ -10,10 +11,12 @@ export class RegiterPageComponent {
 
   private fb = inject(FormBuilder);
   private validatorsService = inject(ValidatorsService);
+  private emailValidator = inject(EmailValidatorService);
 
   public myForm: FormGroup = this.fb.group({
     name: ['', [ Validators.required, Validators.pattern(this.validatorsService.firstNameAndLastnamePattern)] ],
-    email: ['', [ Validators.required, Validators.pattern(this.validatorsService.emailPattern)] ],
+    // email: ['', [ Validators.required, Validators.pattern(this.validatorsService.emailPattern)], [new EmailValidator()] ],
+    email: ['', [ Validators.required, Validators.pattern(this.validatorsService.emailPattern)], [this.emailValidator] ],
     username: ['', [ Validators.required, this.validatorsService.cantBeStrider] ],
     password: ['', [ Validators.required, Validators.minLength(6)] ],
     password2: ['', [ Validators.required] ]
