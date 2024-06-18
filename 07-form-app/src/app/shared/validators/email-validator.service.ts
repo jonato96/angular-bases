d4620da1cd777e1c589eedb96e6476990183b79b
jonato96 <jonato96s@gmail.com>
2024-06-17 20:19:50 -0500

    add-email-validator

--- 07-form-app/src/app/shared/validators/email-validator.service.ts
+++ 07-form-app/src/app/shared/validators/email-validator.service.ts
@@ -7,11 +7,30 @@ export class EmailValidatorService implements AsyncValidator {
 
     validate(control: AbstractControl<any, any>): Observable<ValidationErrors | null> {
         const email = control.value;
-        return of({
-            emailTaken: true
+        
+        const httpCallObservable = new Observable<ValidationErrors|null>( (subscriber) => {
+            if (email === 'jonathan@gmail.com') {
+                subscriber.next({emailTaken: true});
+                subscriber.complete();
+            }
+            subscriber.next(null);
+            subscriber.complete();
+
         }).pipe(
             delay(2000)
         )
+
+        return httpCallObservable;
+        
     }    
+
+    // validate(control: AbstractControl<any, any>): Observable<ValidationErrors | null> {
+    //     const email = control.value;
+    //     return of({
+    //         emailTaken: true
+    //     }).pipe(
+    //         delay(2000)
+    //     )
+    // } 
 
 }
