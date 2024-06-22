2d1b0da2ac86c534c51f01510aeb742dcaa7d9b4
jonato96 <jonato96s@gmail.com>
2024-06-21 19:30:15 -0500

    ajust-format

--- 07-form-app/src/app/auth/auth-routing.module.ts
+++ 07-form-app/src/app/auth/auth-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { RegiterPageComponent } from './pages/regiter-page/regiter-page.component';
 
 const routes: Routes = [
--- 07-form-app/src/app/reactive/reactive.module.ts
+++ 07-form-app/src/app/reactive/reactive.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { ReactiveRoutingModule } from './reactive-routing.module';
+
 import { BasicPageComponent } from './pages/basic-page/basic-page.component';
 import { DynamicPageComponent } from './pages/dynamic-page/dynamic-page.component';
 import { SwitchesPageComponent } from './pages/switches-page/switches-page.component';
