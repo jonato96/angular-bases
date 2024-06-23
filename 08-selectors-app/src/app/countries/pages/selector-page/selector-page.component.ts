442d7eeab0403e64de31a3b0a098d5bf3300bfd6
jonato96 <jonato96s@gmail.com>
2024-06-23 18:30:32 -0500

    add-region-selector

--- 08-selectors-app/src/app/countries/pages/selector-page/selector-page.component.ts
+++ 08-selectors-app/src/app/countries/pages/selector-page/selector-page.component.ts
@@ -1,5 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CountriesService } from '../../services/countries.service';
+import { Region } from '../../interfaces/country.interface';
 
 @Component({
   selector: 'countries-selector-page',
@@ -9,11 +11,16 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class SelectorPageComponent {
 
   private fb = inject(FormBuilder);
+  private countriesService = inject(CountriesService);
 
   public myForm: FormGroup = this.fb.group({
     region: ['', Validators.required],
     country: ['', Validators.required],
     borders: ['', Validators.required],
   });
+
+  get regions(): Region[] {
+    return this.countriesService.regions;
+  }
 
 }
