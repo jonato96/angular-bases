b7861b27b6668c60d2e9209704bb93874723a002
jonato96 <jonato96s@gmail.com>
2024-07-02 16:45:45 -0500

    add-main-map

--- 10-maps-app/src/app/maps/maps.module.ts
+++ 10-maps-app/src/app/maps/maps.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import mapboxgl from 'mapbox-gl';
+(mapboxgl as any).accessToken = environment.mapbox_key;
+
 import { FullScreenPageComponent } from './pages/full-screen-page/full-screen-page.component';
 import { MapsLayoutComponent } from './layout/maps-layout/maps-layout.component';
 import { MapsRoutingModule } from './maps-routing.module';
@@ -9,6 +12,7 @@ import { MiniMapComponent } from './components/mini-map/mini-map.component';
 import { PropertiesPageComponent } from './pages/properties-page/properties-page.component';
 import { SideMenuComponent } from './components/side-menu/side-menu.component';
 import { ZoomRangePageComponent } from './pages/zoom-range-page/zoom-range-page.component';
+import { environment } from '../../environments/environments';
 
 
 @NgModule({
