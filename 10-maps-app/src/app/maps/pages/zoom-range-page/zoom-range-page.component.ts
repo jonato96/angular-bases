804f9540634e03ff8ac23df162a7d83910ef0b25
jonato96 <jonato96s@gmail.com>
2024-07-03 09:26:59 -0500

    add-zoom-control

--- 10-maps-app/src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
+++ 10-maps-app/src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
@@ -11,16 +11,44 @@ export class ZoomRangePageComponent implements AfterViewInit {
   @ViewChild('map')
   public divMap?: ElementRef;
 
+  public zoom: number = 10;
+  public map?: Map;
+
   ngAfterViewInit(): void {
 
     if (!this.divMap) throw 'Element not found';    
 
-    const map = new Map({
+    this.map = new Map({
       container: this.divMap.nativeElement, // container ID
       style: 'mapbox://styles/mapbox/streets-v12', // style URL
       center: [-74.5, 40], // starting position [lng, lat]
-      zoom: 9, // starting zoom
+      zoom: this.zoom, // starting zoom
     });
+    this.mapListerner();
+  }
+
+  mapListerner() {
+    if (!this.map) throw 'Map not init';
+    this.map.on('zoom', (ev) => {
+      this.zoom = this.map!.getZoom();
+    });
+    this.map.on('zoomend', (ev) => {
+      if(this.map!.getZoom() < 18) return;
+      this.map!.zoomTo(18);
+    });
+  }
+
+  zoomIn() {
+    this.map?.zoomIn();
+  }
+
+  zoomOut() {
+    this.map?.zoomOut();
+  }
+
+  zoomChange(value: string) {
+    this.zoom = Number(value);
+    this.map?.zoomTo(this.zoom);
   }
 
 }
