ecf8efcb53317116012ecc6aa69948f33a174f28
jonato96 <jonato96s@gmail.com>
2024-07-07 14:34:28 -0500

    add-standalone-in-other-standalone

--- 10-maps-app/src/app/alone/pages/alone-page/alone-page.component.ts
+++ 10-maps-app/src/app/alone/pages/alone-page/alone-page.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { CounterAloneComponent } from '../../components/counter-alone/counter-alone.component';
 
 @Component({  
   standalone: true,
-  imports: [],
+  imports: [ 
+    CounterAloneComponent
+  ],
   templateUrl: './alone-page.component.html',
   styleUrl: './alone-page.component.css'
 })
