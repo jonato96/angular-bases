7ff8b15e7c75dee9afc61a6e160feb4fe4fd0251
jonato96 <jonato96s@gmail.com>
2024-07-07 14:25:27 -0500

    add-standalone-component

---
+++ 10-maps-app/src/app/alone/pages/alone-page/alone-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({  
+  standalone: true,
+  imports: [],
+  templateUrl: './alone-page.component.html',
+  styleUrl: './alone-page.component.css'
+})
+export class AlonePageComponent {
+
+}
