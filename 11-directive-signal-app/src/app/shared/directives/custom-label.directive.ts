cec9005b2fca9b62eb676dc056c6ae635d78f95d
jonato96 <jonato96s@gmail.com>
2024-07-11 08:42:00 -0500

    add-color-input

--- 11-directive-signal-app/src/app/shared/directives/custom-label.directive.ts
+++ 11-directive-signal-app/src/app/shared/directives/custom-label.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, OnInit } from '@angular/core';
+import { Directive, ElementRef, Input, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[customLabel]'
@@ -6,17 +6,30 @@ import { Directive, ElementRef, OnInit } from '@angular/core';
 export class CustomLabelDirective implements OnInit {
 
   private htmlElement?: ElementRef<HTMLElement>;
+  private _color: string = 'red';
+
+  @Input()
+  set color(value: string) {
+    this._color = value;
+    this.setStyle();
+  }
 
   constructor( private el: ElementRef<HTMLElement>) { 
 
-    console.log(el);
+    // console.log(el);
     
     this.htmlElement = el;
     this.htmlElement.nativeElement.innerHTML = 'Some Info';
+    this.setStyle();
   }
 
   ngOnInit(): void {
     console.log('Directive-OnInit');
+  }
+
+  setStyle(): void {
+    if (!this.htmlElement) return;
+    this.htmlElement.nativeElement.style.color = this._color;
   }
 
 }
