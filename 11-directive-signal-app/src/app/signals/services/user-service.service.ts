ae8f8ed2b8be0f156ae08cfc8b196b982312733e
jonato96 <jonato96s@gmail.com>
2024-07-17 08:36:27 -0500

    add-user-info-structure

---
+++ 11-directive-signal-app/src/app/signals/services/user-service.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { map, Observable, tap } from 'rxjs';
+import { SingleUserReponse, User } from '../interfaces/user-request.interface';
+
+@Injectable({providedIn: 'root'})
+export class UserService {
+    
+    private readonly http = inject(HttpClient);
+
+    private baseUrl: string = 'https://reqres.in/api/users';
+
+    getUserById(id: number): Observable<User> {
+        return this.http.get<SingleUserReponse>(`${this.baseUrl}/${id}`)
+        .pipe(
+            map(response => response.data),
+            tap( console.log ),
+        );
+    }
+    
+}
