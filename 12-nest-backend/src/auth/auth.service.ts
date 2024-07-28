292a44d697190fda536ef3a60b97b4c549af9361
jonato96 <jonato96s@gmail.com>
2024-07-28 15:22:36 -0500

    create-db-register

--- 12-nest-backend/src/auth/auth.service.ts
+++ 12-nest-backend/src/auth/auth.service.ts
@@ -1,11 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { UpdateAuthDto } from './dto/update-auth.dto';
 import { CreateUserDto } from './dto/create-user.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from './entities/user.entity';
+import { Model } from 'mongoose';
+import { Certificate } from 'crypto';
 
 @Injectable()
 export class AuthService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new auth';
+
+  constructor(
+    @InjectModel(User.name)
+    private userModel: Model<User>,
+  ) {}
+
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    // 1 Encriptar pass
+    // 2 Guardar el user
+    // 3 Generar el JWT
+    try {
+      const newUser = new this.userModel(createUserDto);
+      return await newUser.save();
+      
+    } catch(error) {
+      if( error.code === 11000) {
+        throw new BadRequestException(`${createUserDto.email} already exists.`);
+      }
+      throw new InternalServerErrorException(`Something when wrong.`);      
+    }
+
   }
 
   findAll() {
