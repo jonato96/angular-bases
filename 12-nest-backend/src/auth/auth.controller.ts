11b7d2e3dc051504cf304760a6996e77b6c2c535
jonato96 <jonato96s@gmail.com>
2024-08-03 21:02:44 -0500

    generate-new-jwt-token

--- 12-nest-backend/src/auth/auth.controller.ts
+++ 12-nest-backend/src/auth/auth.controller.ts
@@ -3,6 +3,8 @@ import { AuthService } from './auth.service';
 
 import { CreateUserDto, UpdateAuthDto, LoginDto, RegisterUserDto } from './dto';
 import { AuthGuard } from './guards/auth.guard';
+import { User } from './entities/user.entity';
+import { LoginResponse } from './interfaces/login-response';
 
 
 @Controller('auth')
@@ -32,19 +34,31 @@ export class AuthController {
     return this.authService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.authService.findOne(+id);
+  @UseGuards(AuthGuard)
+  @Get('check-token')
+  checkToken( @Request() req: Request): LoginResponse {
+    const user =  req['user'] as User;
+
+    return {
+      user,
+      token: this.authService.getJwtToken({id: user._id})
+    }
+
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAuthDto: UpdateAuthDto) {
-    return this.authService.update(+id, updateAuthDto);
-  }
+  // @Get(':id')
+  // findOne(@Param('id') id: string) {
+  //   return this.authService.findOne(+id);
+  // }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.authService.remove(+id);
-  }
+  // @Patch(':id')
+  // update(@Param('id') id: string, @Body() updateAuthDto: UpdateAuthDto) {
+  //   return this.authService.update(+id, updateAuthDto);
+  // }
+
+  // @Delete(':id')
+  // remove(@Param('id') id: string) {
+  //   return this.authService.remove(+id);
+  // }
   
 }
