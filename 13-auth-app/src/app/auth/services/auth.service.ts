b6acf19db1915c9a75bfc823e870878136047081
jonato96 <jonato96s@gmail.com>
2024-08-11 15:53:54 -0500

    react-to-authentication

--- 13-auth-app/src/app/auth/services/auth.service.ts
+++ 13-auth-app/src/app/auth/services/auth.service.ts
@@ -21,7 +21,9 @@ export class AuthService {
   public currentUser = computed( () => this._currentUser() );
   public authStatus = computed( () => this._authStatus() );
 
-  constructor() { }
+  constructor() { 
+    this.checkAuthStatus().subscribe();
+  }
 
   private setAuthentication(user: User, token: string): boolean {
     this._currentUser.set(user);
