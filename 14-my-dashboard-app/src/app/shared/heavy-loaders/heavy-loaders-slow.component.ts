62c2fd995d462f8a3cb324fc136d5973ca01be29
jonato96 <jonato96s@gmail.com>
2024-08-15 08:59:55 -0500

    add-base-structure

---
+++ 14-my-dashboard-app/src/app/shared/heavy-loaders/heavy-loaders-slow.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heavy-loader-fast',
+  standalone: true,
+  imports: [],
+  template: `
+    <h1> Hola Mundo </h1>
+  `,
+  styles: ``
+})
+export class HeavyLoadersFastComponent {
+
+}
