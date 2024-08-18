1a8224fd8feafe83e7b1d173f4a0d56f35b3651e
jonato96 <jonato96s@gmail.com>
2024-08-17 20:37:21 -0500

    add-side-menu

--- 14-my-dashboard-app/src/app/shared/side-menu/side-menu.component.ts
+++ 14-my-dashboard-app/src/app/shared/side-menu/side-menu.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { routes } from '../../app.routes';
 
 @Component({
   selector: 'app-side-menu',
@@ -8,5 +9,23 @@ import { Component } from '@angular/core';
   styleUrl: './side-menu.component.css'
 })
 export class SideMenuComponent {
+
+  public menuItems = routes
+  .map( route => route.children ?? [])
+  .flat()
+  .filter( route => route && route.path )
+  .filter( route => !route.path?.includes(':'))
+
+  constructor() {
+
+    // const dashboardRoutes = routes
+    // .map( route => route.children ?? [])
+    // .flat()
+    // .filter( route => route && route.path )
+    // .filter( route => !route.path?.includes(':'))
+
+    // console.log(dashboardRoutes)
+
+  }
 
 }
