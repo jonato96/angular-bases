26a2c52a5c9099ed89c4872920c09620e2fcbf07
jonato96 <jonato96s@gmail.com>
2024-08-17 20:48:22 -0500

    add-dinamic-menu

--- 14-my-dashboard-app/src/app/shared/side-menu/side-menu.component.ts
+++ 14-my-dashboard-app/src/app/shared/side-menu/side-menu.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { routes } from '../../app.routes';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-side-menu',
   standalone: true,
-  imports: [],
+  imports: [ CommonModule, RouterModule ],
   templateUrl: './side-menu.component.html',
   styleUrl: './side-menu.component.css'
 })
