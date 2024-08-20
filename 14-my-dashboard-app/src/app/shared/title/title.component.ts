e830c345e5758a3c396903dee09ab2516ed05f6e
jonato96 <jonato96s@gmail.com>
2024-08-19 21:01:48 -0500

    add-required-input

--- 14-my-dashboard-app/src/app/shared/title/title.component.ts
+++ 14-my-dashboard-app/src/app/shared/title/title.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { booleanAttribute, Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-title',
   standalone: true,
   imports: [],
-  templateUrl: './title.component.html',
+  template:  `
+    <h1 class="text-3xl mb-5"> {{ title }} - {{ withShadow }} </h1>
+  `,
   styles: ``
 })
 export class TitleComponent {
+
+  @Input({required: true})
+  title!: string;
+
+  @Input({transform: booleanAttribute})
+  withShadow: boolean = false;
 
 }
