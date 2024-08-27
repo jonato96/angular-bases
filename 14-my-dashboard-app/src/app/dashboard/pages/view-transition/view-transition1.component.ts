de22d53166b591dd4f31558bfc989467162e0679
jonato96 <jonato96s@gmail.com>
2024-08-26 20:19:13 -0500

    add-hero-animation

--- 14-my-dashboard-app/src/app/dashboard/pages/view-transition/view-transition1.component.ts
+++ 14-my-dashboard-app/src/app/dashboard/pages/view-transition/view-transition1.component.ts
@@ -13,8 +13,9 @@ import { TitleComponent } from '@shared/title/title.component';
        alt = "Picsum"
        width = "200"
        height= "300"
+       style="view-transition-name: hero1"
       />
-      <div class="bg-blue-500 w-56 h-56">
+      <div class="bg-blue-500 w-56 h-56" style="view-transition-name: hero2">
 
       </div>
 
