ab402043c0db670fcb6506c34f49ca0f6b78d127
jonato96 <jonato96s@gmail.com>
2024-09-01 09:19:35 -0500

    add-material-components

--- 14-my-dashboard-app/src/app/dashboard/pages/material/material.component.ts
+++ 14-my-dashboard-app/src/app/dashboard/pages/material/material.component.ts
@@ -1,12 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatBadgeModule } from '@angular/material/badge';
+
 
 @Component({
   selector: 'app-material',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    MatSlideToggleModule,
+    MatIconModule,
+    MatButtonModule,
+    MatBadgeModule
+  ],
   templateUrl: './material.component.html',
   styles: ``
 })
 export default class MaterialComponent {
+
+
 
 }
